6695eee9f87ea2368358ef2e47b6a06cf12b21aa
Vladimir Ivanov <v.ivanov@trueengineering.ru>
2019-02-24 15:27:41 +0700

    Unit tests. Api mapper test cases.

---
+++ app/src/test/kotlin/com/vivanov/currencyconverter/data/network/mappers/ApiMapperTestCases.kt
@@ -0,0 +1,55 @@
+package com.vivanov.currencyconverter.data.network.mappers
+
+import com.vivanov.currenciesconverter.data.network.mappers.ApiMapper
+import com.vivanov.currenciesconverter.data.network.model.responses.CurrenciesResponse
+import com.vivanov.currenciesconverter.domain.model.Currency
+import org.junit.jupiter.api.Assertions
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object ApiMapperTestCases : Spek({
+
+    describe("ApiMapper tests") {
+
+        val apiMapper = ApiMapper()
+
+        val validCurrencyCode0 = Currency.EUR.name
+        val validCurrencyRate0 = 70.0
+        val validCurrencyCode1 = Currency.USD.name
+        val validCurrencyRate1 = 60.0
+        val validCurrencyCode2 = Currency.RUB.name
+        val validCurrencyRate2 = 1.0
+        val invalidCurrencyCode = "AA"
+        val invalidCurrencyRate = 0.0
+
+        val currencies2of3 = hashMapOf(
+            validCurrencyCode0 to validCurrencyRate0,
+            validCurrencyCode1 to validCurrencyRate1,
+            invalidCurrencyCode to invalidCurrencyRate
+        )
+        val validCurrenciesResponse2of3 = CurrenciesResponse(
+            "", "", currencies2of3
+        )
+
+        val currencies3of3 = hashMapOf(
+            validCurrencyCode0 to validCurrencyRate0,
+            validCurrencyCode1 to validCurrencyRate1,
+            validCurrencyCode2 to validCurrencyRate2
+        )
+
+        val validCurrenciesResponse3of3 = CurrenciesResponse(
+            "", "", currencies3of3
+        )
+
+        describe("map currenciesResponse validation") {
+
+            it("check 2 of currencies after validation ") {
+                Assertions.assertTrue(apiMapper.map(validCurrenciesResponse2of3).size == 2)
+            }
+
+            it("check 3 of currencies after validation") {
+                Assertions.assertTrue(apiMapper.map(validCurrenciesResponse3of3).size == 3)
+            }
+        }
+    }
+})
